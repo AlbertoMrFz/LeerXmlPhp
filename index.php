b3b7560b699e9ebf3bc38ff0bfb22a436a16aee4
AlbertoMrFz <brtandhhh1595@gmail.com>
2016-04-20 10:57:14 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+function leerXml($urlXml)
+{
+	$fichero = file_get_contents($urlFlujo);
+	$xmlDOM = new DOMDocument('1.0', 'utf-8');
+	$xmlDOM->loadXML($fichero);
+	$documentElement = $xmlDOM->documentElement;
+	return $documentElement;        
+}
+
+$url = "http://feeds.weblogssl.com/xataka2"
+$contenidoDom = leerXml($url);
+
+$item = $contenidoDom->getElementsByTagName("item");
+for ($i = 0; $i < $item->length; $i++)
+{
+	$titulo = $item->item($i)->getElementsByTagName("title");
+	$enlace = $item->item($i)->getElementsByTagName("link");
+	$fecha = $item->item($i)->getElementsByTagName("pubDate");
+	$contenido = $item->item($i)->getElementsByTagName("description");
+	
+	$tituloXml = strip_tags(($titulo->item($a)->nodeValue));
+	$enlaceXml = $enlace->item($a)->nodeValue;
+	$contenidoXml = strip_tags($contenido->item($a)->nodeValue);
+	$fechaXml = $fecha->item($a)->nodeValue;
+}
+	
+?>
